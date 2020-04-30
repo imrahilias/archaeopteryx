5b1fc09525c318556fe75e5a8d8ed74ba4d0f533
imrahilias <siegel.moritz@gmail.com>
2020-04-30 12:09:43 +0200

    slurm

---
+++ zephyr/update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## backup stray config files
+
+## rsnapshot timers
+sudo cp /etc/systemd/system/rsnapshot-weekly.timer ~/zephyr/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-daily.timer ~/zephyr/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-hourly.timer ~/zephyr/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-monthly.timer ~/zephyr/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot@.service ~/zephyr/rsnapshot
+
+## others
+sudo cp /etc/mkinitcpio.conf ~/zephyr
+sudo cp /etc/pacman.d/hooks/nvidia.hook ~/zephyr
+sudo cp /etc/pacman.conf ~/zephyr
+sudo cp /etc/powerpill/powerpill.json ~/zephyr
+sudo cp /etc/rsnapshot.conf ~/zephyr
+sudo cp /etc/slurm-llnl/slurm.conf ~/zephyr
