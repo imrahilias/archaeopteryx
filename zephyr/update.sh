5ea0d99e9667751dade8b72b8f012e3777f1bf71
Imrahilias <siegel.moritz@tuwien.ac.at>
2023-03-19 17:00:03 +0100

    colibri update

--- zephyr/update.sh
+++ zephyr/update.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 ## backup stray config files
 
+mydir="/home/m/zephyr/zephyr/$(hostname)"
+echo $mydir
+
 ## rsnapshot timers
-sudo cp /etc/systemd/system/rsnapshot-weekly.timer ~/zephyr/rsnapshot
-sudo cp /etc/systemd/system/rsnapshot-daily.timer ~/zephyr/rsnapshot
-sudo cp /etc/systemd/system/rsnapshot-hourly.timer ~/zephyr/rsnapshot
-sudo cp /etc/systemd/system/rsnapshot-monthly.timer ~/zephyr/rsnapshot
-sudo cp /etc/systemd/system/rsnapshot@.service ~/zephyr/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-weekly.timer $mydir/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-daily.timer $mydir/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-hourly.timer $mydir/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot-monthly.timer $mydir/rsnapshot
+sudo cp /etc/systemd/system/rsnapshot@.service $mydir/rsnapshot
 
 ## others
-sudo cp /etc/mkinitcpio.conf ~/zephyr
-sudo cp /etc/pacman.d/hooks/nvidia.hook ~/zephyr
-sudo cp /etc/pacman.conf ~/zephyr
-sudo cp /etc/powerpill/powerpill.json ~/zephyr
-sudo cp /etc/rsnapshot.conf ~/zephyr
-sudo cp /etc/fstab ~/zephyr
-sudo cp /etc/X11/xorg.conf ~/zephyr
-sudo cp /etc/profile ~/zephyr
+sudo cp /etc/mkinitcpio.conf $mydir/
+sudo cp /etc/pacman.d/hooks/nvidia.hook $mydir/
+sudo cp /etc/pacman.conf $mydir/
+sudo cp /etc/powerpill/powerpill.json $mydir/
+sudo cp /etc/rsnapshot.conf $mydir/
+sudo cp /etc/fstab $mydir/
+sudo cp /etc/X11/xorg.conf $mydir/
+sudo cp /etc/profile $mydir/
